d7307e268c4c642ee27302c7edd94e7095cd188e
Kinixa <kinga.korycka@o2.pl>
2023-01-10 21:14:28 +0100

    C;

---
+++ ProjectUnity/Assets/C#/MovmentPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovmentPlayer : MonoBehaviour
+{
+    public float speed = 1f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hdirection;
+        float vdirection;
+        if ((hdirection = Input.GetAxis("Horizontal")) !=0)
+        {
+           rb.AddForce(hdirection * Time.deltaTime * speed, 0, 0);
+        }
+
+        if ((vdirection = Input.GetAxis("Vertical")) !=0)
+        {
+           rb.AddForce( 0, 0, vdirection * Time.deltaTime * speed);
+        }
+    }
+}
